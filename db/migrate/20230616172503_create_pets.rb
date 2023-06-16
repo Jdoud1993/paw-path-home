32e4b59229e636a8ebe849058f8b6cd4ffa1f49f
Joshua Doud <bigjj3393@yahoo.com>
2023-06-16 10:34:53 -0700

    Added bootstrap, created models, updated gem/db

---
+++ db/migrate/20230616172503_create_pets.rb
@@ -0,0 +1,15 @@
+class CreatePets < ActiveRecord::Migration[6.1]
+  def change
+    create_table :pets do |t|
+      t.string :name
+      t.string :species
+      t.string :breed
+      t.string :sex
+      t.string :lost_or_found
+      t.string :phone_number
+      t.integer :user_id
+
+      t.timestamps
+    end
+  end
+end
