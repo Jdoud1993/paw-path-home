d8036d6677dcc591e09142d5394738212694a1af
Joshua Doud <bigjj3393@yahoo.com>
2023-06-30 16:20:48 -0700

    serializer for pet detail to include comments

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -7,7 +7,7 @@ class PetsController < ApplicationController
     def show
         pet = Pet.find_by(id: params[:id])
         if pet
-            render json: pet, include: ["comments"]
+            render json: pet, serializer: PetDetailSerializer
         else
             render json: {error: "Pet not found"}, status: :not_found 
         end 
