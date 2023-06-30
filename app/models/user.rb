dde437cb705d21a265267e3a1fe89cfc8d2f3141
Joshua Doud <bigjj3393@yahoo.com>
2023-06-30 13:35:11 -0700

    password length validation and error

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
     has_secure_password
 
     validates :username, presence: true, uniqueness: true
+    validates :password, length: {in: 6..20}
 
     has_many :comments
     has_many :posted_pets, :class_name => "Pet", :foreign_key => "user_id"
